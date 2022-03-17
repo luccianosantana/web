feef9f82aa4349705f71f329b3b5cf75ab32c517
luccianosantana <lucianosantana@gmail.com>
2022-03-17 12:01:56 -0300

    Adicionado botao para exportar via CSV

--- inventario/urls.py
+++ inventario/urls.py
@@ -10,4 +10,5 @@ urlpatterns =[
     path("update/<int:pk>/", views.ServidorUpdate.as_view(), name="update"),
     path("detail/<int:pk>/", views.ServidorDetail.as_view(), name="detail"),
     path("delete/<int:pk>/", views.ServidorDelete.as_view(), name="delete"),
+    path('export/csv-database-write/', views.csv_database_write, name='csv_database_write'),
 ]
--- inventario/views.py
+++ inventario/views.py
@@ -1,8 +1,27 @@
+import csv
 from django.urls import reverse_lazy
+from django.http import HttpResponse
 from django.views.generic import ListView, CreateView, UpdateView, DetailView
 from django.views.generic.edit import DeleteView
 from django.shortcuts import render
 from inventario.models import Servidor
+
+def csv_database_write(request):
+    
+    # Get all data from UserDetail Databse Table
+    servidores = Servidor.objects.all()
+
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="csv_database_write.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Nome', 'Ambiente', 'Responsavel', 'Dominio'])
+
+    for server in servidores:
+        writer.writerow([server.nome, server.ambiente, server.responsavel, server.dominio])
+
+    return response
 
 class ServidorList(ListView):
     model = Servidor
